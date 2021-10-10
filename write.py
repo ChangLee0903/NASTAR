eb6470ee20563823b65d28edb10c9c0b475cd353
Chi-Chang Lee <r08922a27@csie.ntu.edu.tw>
2021-10-11 01:10:56 +0800

    final

--- write.py
+++ write.py
@@ -1,13 +1,13 @@
 import csv
 import torch
 
-for noise_type in ['ACVacuum_7', 'Babble_7', 'CafeRestaurant_7', 'Car_7', 'MetroSubway_7']:
+for noise_type in ['ACVacuum', 'Babble', 'CafeRestaurant', 'Car', 'MetroSubway']:
     results = torch.load(f'vcb_table/results_{noise_type}.pth')
-    with open(f'vcb_table/{noise_type}_new.csv', 'w', newline='') as csvfile:
+    with open(f'vcb_table/{noise_type}.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(
             ['method'] + [m for m in results[noise_type]['PTN'] if m != 'loss'])
 
-        for method in ['PTN', 'DAT_one_vcb', 'DAT_full_vcb', 'TEST', 'EXTR', 'GT', 'ALL_A09', 'RETV', 'NASTAR_A09_K250']:
+        for method in ['PTN', 'DAT_one', 'DAT_full', 'OPT', 'EXTR', 'GT', 'ALL', 'RETV', 'NASTAR']:
             writer.writerow([method] + ['{:.4f}'.format(results[noise_type][method][m])
                                         for m in results[noise_type][method] if m != 'loss'])
