54184efb842869afe9b671a3888a53fb8da783f2
Chi-Chang Lee <r08922a27@csie.ntu.edu.tw>
2021-10-12 00:20:53 +0800

    upload layer

---
+++ layer.py
@@ -0,0 +1,100 @@
+from torchaudio.functional import magphase
+from functools import partial
+import torch
+
+
+class OnlinePreprocessor(torch.nn.Module):
+    def __init__(self, win_len=512, hop_len=256, n_freq=257, log=True, eps=1e-10, **kwargs):
+        super(OnlinePreprocessor, self).__init__()
+        n_fft = (n_freq - 1) * 2
+        self._win_args = {'n_fft': n_fft,
+                          'hop_length': hop_len, 'win_length': win_len}
+        self.register_buffer('_window', torch.hann_window(win_len))
+
+        self._stft_args = {'center': True, 'pad_mode': 'reflect',
+                           'normalized': False, 'onesided': True}
+        self._istft_args = {'center': True,
+                            'normalized': False, 'onesided': True}
+
+        self._stft = partial(torch.stft, **self._win_args, **self._stft_args)
+        self._istft = partial(
+            torch.istft, **self._win_args, **self._istft_args)
+        self._magphase = partial(magphase, power=2)
+
+        self.log = log
+        self.eps = eps
+
+    def forward(self, wavs):
+        complx = self._stft(wavs, window=self._window)
+        feats, phases = self._magphase(complx)
+        feats = feats.sqrt()
+
+        if self.log:
+            feats = (feats + self.eps).log()
+
+        feats = feats.permute(0, 2, 1).contiguous()
+        return feats, phases
+
+    def inverse(self, feats, phases, length=None):
+        feats = feats.permute(0, 2, 1).contiguous()
+
+        if self.log:
+            feats = feats.exp()
+
+        complxs = feats.unsqueeze(-1) * \
+            torch.stack([phases.cos(), phases.sin()], dim=-1)
+        return self._istft(complxs, window=self._window, length=length)
+
+
+class Conv1dBlock(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, activation='Identity'):
+        super(Conv1dBlock, self).__init__()
+        self.conv = torch.nn.Conv1d(in_channels=in_channels, out_channels=out_channels,
+                                    kernel_size=kernel_size, padding=kernel_size//2)
+        self.act = eval(f'torch.nn.{activation}()')
+
+    def forward(self, features):
+        predicted = self.conv(features)
+        predicted = self.act(predicted)
+        return predicted
+
+
+class LSTM(torch.nn.Module):
+    def __init__(self, feat_size, hidden_size, num_layers, bidirectional, **kwargs):
+        super(LSTM, self).__init__()
+        self._lstm = torch.nn.LSTM(input_size=feat_size, hidden_size=hidden_size,
+                                   num_layers=num_layers, batch_first=True,
+                                   bidirectional=bidirectional)
+        if bidirectional:
+            hidden_size = 2 * hidden_size
+        self.scaling_layer = torch.nn.Linear(hidden_size, feat_size)
+
+    def forward(self, specs):
+        predicted, _ = self._lstm(specs)
+        predicted = self.scaling_layer(predicted)
+        predicted = torch.clamp(predicted, min=0)
+        return predicted
+
+
+class GRU(torch.nn.Module):
+    def __init__(self, feat_size, hidden_size, num_layers, bidirectional, **kwargs):
+        super(GRU, self).__init__()
+        self._gru = torch.nn.GRU(input_size=feat_size, hidden_size=hidden_size,
+                                 num_layers=num_layers, batch_first=True,
+                                 bidirectional=bidirectional)
+        if bidirectional:
+            hidden_size = 2 * hidden_size
+        self.scaling_layer = torch.nn.Linear(hidden_size, feat_size)
+
+    def forward(self, specs):
+        predicted, _ = self._gru(specs)
+        predicted = self.scaling_layer(predicted)
+        predicted = torch.clamp(predicted, min=0)
+        return predicted
+
+m = OnlinePreprocessor()
+wav_i = torch.zeros(1, 16000)
+mag, pha = m(wav_i)
+wav_o = m.inverse(mag, pha, 16000)
+print(wav_i[:16000])
+print(wav_o[:16000])
--- model.py
+++ model.py
@@ -30,19 +30,76 @@ def load_model(args):
     return args, model, optimizer, current_step
 
 
-class Conv1dBlock(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, spectral_norm=False, activation='Identity'):
-        super(Conv1dBlock, self).__init__()
-        self.conv = torch.nn.Conv1d(in_channels=in_channels, out_channels=out_channels,
-                                    kernel_size=kernel_size, padding=kernel_size//2)
-        if spectral_norm:
-            self.conv = torch.nn.utils.spectral_norm(self.conv)
-        self.act = eval(f'torch.nn.{activation}()')
+class SpectrumDenoiseModel(torch.nn.Module):
+    def __init__(self, args, **kwargs):
+        super(SpectrumDenoiseModel, self).__init__()
+        from layer import OnlinePreprocessor
+        self.preprocessor = OnlinePreprocessor(
+            **args.config['model'][args.model]['feat'])
+        feat_size = args.config['model'][args.model]['feat']['n_freq']
+        if args.ae_ckpt is None:
+            if args.model == 'LSTM':
+                from layer import LSTM
+                self.ae_model = LSTM(feat_size=feat_size,
+                                     **args.config['model']['LSTM'])
+            elif args.model == 'GRU':
+                from layer import GRU
+                self.ae_model = GRU(feat_size=feat_size,
+                                    **args.config['model']['GRU'])
 
-    def forward(self, features):
-        predicted = self.conv(features)
-        predicted = self.act(predicted)
-        return predicted
+            self.init_weights()
+        else:
+            self.ae_model = torch.load(args.ae_ckpt)
+
+    @ torch.no_grad()
+    def _get_length_masks(self, lengths):
+        # lengths: (batch_size, ) in cuda
+        ascending = torch.arange(MAX_POSITIONS_LEN)[:lengths.max().item()].unsqueeze(
+            0).expand(len(lengths), -1)
+        length_masks = (ascending < lengths.unsqueeze(-1))
+        return length_masks
+
+    def init_weights(self):
+        for name, param in self.named_parameters():
+            if 'weight_hh' in name:
+                torch.nn.init.orthogonal_(param.data)
+            elif 'weight' in name:
+                if param.data.ndim == 1:
+                    param.data = param.data.unsqueeze(-1)
+                    torch.nn.init.xavier_uniform_(param.data)
+                    param.data = param.data.squeeze(-1)
+                else:
+                    torch.nn.init.xavier_uniform_(param.data)
+            elif 'bias' in name:
+                torch.nn.init.constant_(param.data, 0)
+
+    @ torch.no_grad()
+    def _mask(self, feat, lengths):
+        stft_lengths = lengths // self.preprocessor._win_args['hop_length'] + 1
+        stft_length_masks = self._get_length_masks(stft_lengths)
+        feat = feat * stft_length_masks
+        return feat
+
+    def forward(self, wav, target, lengths, loss_fn):
+        with torch.no_grad():
+            feat_tar, _ = self.preprocessor(target)
+            if not lengths is None:
+                feat_tar = self._mask(feat_tar, lengths)
+
+        predicted = self.transform(wav, lengths, True)
+        loss = loss_fn(predicted.flatten(start_dim=1).contiguous(),
+                       feat_tar.flatten(start_dim=1).contiguous())
+        return loss
+
+    def transform(self,  wav, lengths=None, istrain=False):
+        with torch.no_grad():
+            feat_inp, phase_inp = self.preprocessor(wav)
+        predicted = self.ae_model(feat_inp)
+
+        if not lengths is None:
+            predicted = self._mask(predicted, lengths)
+        return predicted if istrain \
+            else self.preprocessor.inverse(predicted, phase_inp, wav.shape[-1])
 
 
 class DenoiseModel(torch.nn.Module):
@@ -52,13 +109,10 @@ class DenoiseModel(torch.nn.Module):
             if args.model == 'DEMUCS':
                 from denoiser.demucs import Demucs as DEMUCS
                 self.ae_model = DEMUCS(**args.config['model']['DEMUCS'])
-                self.init_weights()
-            elif args.model == 'LSTM':
-                self.ae_model = DEMUCS(**args.config['model']['DEMUCS'])
-                self.init_weights()
+            self.init_weights()
         else:
             self.ae_model = torch.load(args.ae_ckpt)
- 
+
     @ torch.no_grad()
     def _get_length_masks(self, lengths):
         # lengths: (batch_size, ) in cuda
