a3ed885794ee42bc527f2904e742cc40772934d2
Chi-Chang Lee <r08922a27@csie.ntu.edu.tw>
2021-10-13 20:10:42 +0800

    fix EXTR script

--- train_EXTR.sh
+++ train_EXTR.sh
@@ -2,3 +2,4 @@ for NOISE_TYPE in ACVacuum Babble CafeRestaurant Car MetroSubway; do
     python main.py --task train --method EXTR \
     --target_noise target_data/${NOISE_TYPE}/pseudo.wav \
     --eval_noise target_data/${NOISE_TYPE}/train.wav
+done
